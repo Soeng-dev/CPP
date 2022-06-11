3cbb49fbb87f922dd1209c952bc4d1c2f0deea7a
yong-dev <tjddyd1565@naver.com>
2022-06-11 18:40:54 +0900

    mod01 ex06 done

---
+++ cpp01/ex06/main.cpp
@@ -0,0 +1,17 @@
+
+#include "Harl.hpp"
+
+int	main(int argc, char **argv)
+{
+	Harl harl;
+
+	if (argc != 2)
+	{
+		std::cout << "Wrong input" << std::endl;
+		return (-1);
+	}
+
+	harl.complain(argv[1]);
+
+	return (0);
+}
